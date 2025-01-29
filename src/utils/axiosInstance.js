e8cc4015e7e755f08460fe175b1f8b2431b67d3a
Apple <apple@Apples-MacBook-Air.local>
2025-01-29 12:57:40 +0530

    prod axios baseURL added

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000",
+    // baseURL: "http://localhost:8000",
+    baseURL: "https://task-manager-ass.vercel.app"
 })
 axiosInstance.interceptors.request.use((config) => {
     const token = localStorage.getItem('authToken');
